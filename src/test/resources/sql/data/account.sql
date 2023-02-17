86306cff0e6c7d87da27db6938e2d748a02e1315
devjun10 <devjun1023@gmail.com>
2023-02-17 11:22:34 +0900

    build: 데이터.sql 추가
    - 간략한 테스트를 위해 데이터.sql 추가

---
+++ src/test/resources/sql/data/account.sql
@@ -0,0 +1,10 @@
+INSERT INTO account
+VALUES (1);
+INSERT INTO account
+VALUES (2);
+INSERT INTO account
+VALUES (3);
+INSERT INTO account
+VALUES (4);
+INSERT INTO account
+VALUES (5);
---
+++ src/test/resources/sql/data/activity.sql
@@ -0,0 +1,6 @@
+INSERT INTO weather.activity (id, amount, owner_account_id, source_account_id, target_account_id, timestamp)
+VALUES (1, 10000, 2, 2, 1, '2023-02-17 10:08:19');
+INSERT INTO weather.activity (id, amount, owner_account_id, source_account_id, target_account_id, timestamp)
+VALUES (2, 100000, 1, 2, 1, '2023-02-17 10:08:19');
+INSERT INTO weather.activity (id, amount, owner_account_id, source_account_id, target_account_id, timestamp)
+VALUES (3, 1000000, 3, 3, 2, '2023-02-17 10:08:19');
